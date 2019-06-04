af807856aee2c53d60f4181720fdea1b44d25a1f
Andre Xavier <andre.macedo@aluno.positivo.edu.br>
2019-06-04 14:39:22 -0300

    Add Campeonato e Equipe

---
+++ ArqDota2/Models/Campeonato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArqDota2.Models
+{
+    namespace ArqDota2.Models
+    {
+        public class Campeonato
+        {
+            public string Duracao { get; set; }
+            public DateTime DataCampeonato { get; set; }
+
+        }
+    }
+}
---
+++ ArqDota2/Models/Equipe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArqDota2.Models
+{
+    namespace ArqDota2.Models
+    {
+        public class Equipe
+        {
+            public void AdicionarEquipe()
+            {
+                //
+            }
+            public void EditarEquipe()
+            {
+                //
+            }
+            public void ExcluirEquipe()
+            {
+                //
+            }
+            public void VisualizarEquipe()
+            {
+                //
+            }
+            public void ExibirHistoricoEdicao()
+            {
+                //DemonstrarDadosEditados --> Exibir quais jogadores foram editados e quando [data].
+            }
+        }
+    }
+}
